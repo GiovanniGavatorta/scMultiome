1a10e6398454b1e6174a2d42917f14e97f1dfdd3
Giovanni Gavatorta <giovanni.gavatorta02@gmail.com>
2025-06-23 19:33:57 +0200

    Add SplitGeneAndAtac function with full documentation and examples

---
+++ R/Step2_SplitGeneExprAndATACseqData.R
@@ -0,0 +1,41 @@
+#' Split Gene Expression and ATAC-seq Data
+#'
+#' This function performs Step 2 of the exam:
+#' It splits the full `data.table` from Step 1 into two separate tables:
+#' one for gene expression (rows starting with "ENSG") and one for ATAC-seq peaks
+#' (rows starting with "chr").
+#'
+#' @param dt A `data.table` created by `ConvertMatrix()` with a column called `"row.names"` 
+#'          containing either Ensembl gene IDs or genomic peak coordinates.
+#'
+#' @return A list with two named elements:
+#' \describe{
+#'   \item{gene_expr}{A `data.table` of gene expression rows (Ensembl gene IDs).}
+#'   \item{atac_peaks}{A `data.table` of ATAC-seq peak rows (chromosome coordinates).}
+#' }
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Assuming expr_dt is the output from ConvertMatrix()
+#' split_data <- SplitGeneAndAtac(expr_dt)
+#' gene_expr_dt <- split_data$gene_expr
+#' atac_peaks_dt <- split_data$atac_peaks
+#'
+#' head(gene_expr_dt)
+#' head(atac_peaks_dt)
+#' }
+SplitGeneAndAtac <- function(dt) {
+  
+  row_ids <- dt[[1]]  
+  
+  gene <- grepl("^ENSG", row_ids)
+  peak <- grepl("^chr", row_ids)
+  
+  gene_expr <- dt[gene, ]
+  atac_peaks <- dt[peak, ]
+  
+  return(list(gene_expr = gene_expr, atac_peaks = atac_peaks))
+}
+
